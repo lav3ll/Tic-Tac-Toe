5ebbae7de40f9a15674eef6943cee95d6c5aa63a
lav3ll <lavell.francis@hotmail.com>
2022-11-27 21:32:06 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const o = document.querySelector('.o');
+const x = document.querySelector('.x');
+const ticContainer = document.querySelector('.ticContainer');
+const choose = document.querySelector('.choose')
+
+
+//
+ticContainer.addEventListener('click', function () {
+    let counter = 0;
+    choose.classList.remove('visible');
+    x.classList.remove('visible');
+    o.classList.remove('visible');
+});
+
+x.addEventListener('click', function () { })
+
+
